591b5d29f84da11ceb7039b1a0a8076a16fd611a
Rares <6453351+raress96@users.noreply.github.com>
2021-06-26 19:53:25 +0300

    Working memes creator contract, which creates an nft and sends it to the user.

--- memes-creator/src/lib.rs
+++ memes-creator/src/lib.rs
@@ -4,25 +4,26 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
+const TRIPA_NFT: &[u8] = "TRIPA-bb7f5c".as_bytes();
+
 #[elrond_wasm_derive::contract]
-pub trait Adder {
-	#[view(getSum)]
-	#[storage_get("sum")]
-	fn get_sum(&self) -> Self::BigUint;
-
-	#[storage_set("sum")]
-	fn set_sum(&self, sum: Self::BigUint);
-
+pub trait MemesCreator {
 	#[init]
-	fn init(&self, initial_value: Self::BigUint) {
-		self.set_sum(initial_value);
-	}
+	fn init(&self) {}
 
 	#[endpoint]
-	fn add(&self, value: Self::BigUint) -> SCResult<()> {
-		let mut sum = self.get_sum();
-		sum += value;
-		self.set_sum(sum);
+	fn create_meme(&self, name: BoxedBytes, url: BoxedBytes) -> SCResult<()> {
+		let amount: &Self::BigUint = &Self::BigUint::from(1u64);
+		let royalties: &Self::BigUint = &Self::BigUint::from(500u64);
+		let address: Address = self.blockchain().get_caller();
+		let sc_address: Address = self.blockchain().get_sc_address();
+		let nft_token: TokenIdentifier = TokenIdentifier::from(TRIPA_NFT);
+		let hash: &BoxedBytes = &BoxedBytes::empty();
+
+		self.send().esdt_nft_create(&nft_token, &amount, &name, royalties, hash, &{}, &[url]);
+
+		let nonce: u64 = self.blockchain().get_current_esdt_nft_nonce(&sc_address, &nft_token);
+		self.send().direct_nft(&address, &nft_token, nonce, amount, &[]);
 
 		Ok(())
 	}
