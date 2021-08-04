3e99076cd36fc2a00b1e59a09d227477a3dd8c9d
Rares <6453351+raress96@users.noreply.github.com>
2021-08-04 23:28:22 +0300

    Fully working top memes implementation with tests.

--- memes-voting/src/lib.rs
+++ memes-voting/src/lib.rs
@@ -2,15 +2,15 @@
 #![allow(unused_attributes)]
 
 elrond_wasm::imports!();
-elrond_wasm::derive_imports!();
 
 pub mod meme;
 use meme::*;
 
 const PER_PAGE: usize = 10;
 const PERIOD_TIME: u64 = 604800; // 1 week in seconds
+const VOTES_PER_ADDRESS_PER_PERIOD: u8 = 20;
 
-#[elrond_wasm_derive::contract]
+#[elrond_wasm::contract]
 pub trait MemesVoting {
 	#[init]
 	fn init(&self, creator_contract: &Address, period: &u64) {
@@ -45,20 +45,29 @@ pub trait MemesVoting {
 
 		if block_timestamp > period && block_timestamp - period >= PERIOD_TIME {
 			self.periods().push(&block_timestamp);
-			// TODO: Calculate top 10 memes and save them
+			// TODO: Send Top memes to other contract
 		}
 	}
 
 	#[endpoint]
 	fn vote_memes(&self, nft_nonces: &[u64]) -> SCResult<()> {
-		// let caller: Address = self.blockchain().get_caller();
-		// require!(
-		// 	TODO: Add condition,
-		// 	"Not enough votes left currently"
-		// );
+		let caller: Address = self.blockchain().get_caller();
 
+		self.alter_period();
+
+		let address_votes: SingleValueMapper<Self::Storage, AddressVotes> = self.address_votes(caller);
+		let current_period: u64 = self.current_period();
+		let nb_nfts: usize = nft_nonces.len();
+		let reset_address_votes = address_votes.is_empty() || address_votes.get().period != self.current_period();
+
+		require!(
+			reset_address_votes || (address_votes.get().votes >= nb_nfts as u8),
+			"Not enough votes left currently"
+		);
+
+		let mut meme_votes: Vec<MemeVotes> = Vec::new();
 		for nonce in nft_nonces {
-			let current_votes: SingleValueMapper<Self::Storage, u32> = self.meme_votes(*nonce);
+			let current_votes: SingleValueMapper<Self::Storage, u32> = self.meme_votes(*nonce, current_period);
 
 			let mut votes = 0;
 			if !current_votes.is_empty() {
@@ -67,9 +76,45 @@ pub trait MemesVoting {
 
 			votes += 1;
 			current_votes.set(&votes);
+			meme_votes.push(MemeVotes {
+				nft_nonce: *nonce,
+				votes,
+			});
 		}
 
+		address_votes.set(&AddressVotes {
+			period: current_period,
+			votes: (if reset_address_votes { VOTES_PER_ADDRESS_PER_PERIOD } else { address_votes.get().votes }) - (nb_nfts as u8),
+		});
+
+		self.alter_period_top_memes(&mut meme_votes);
+
 		Ok(())
+	}
+
+	fn alter_period_top_memes(&self, meme_votes: &mut Vec<MemeVotes>) {
+		let current_period: u64 = self.current_period();
+		let top_memes: SingleValueMapper<Self::Storage, Vec<MemeVotes>> = self.period_top_memes(current_period);
+
+		if !top_memes.is_empty() {
+			meme_votes.append(&mut top_memes.get());
+		}
+
+		// Sort first by nonce and then by votes, higher nonce with higher votes first
+		meme_votes.sort_unstable_by(|a, b| b.nft_nonce.cmp(&a.nft_nonce).then_with(|| b.votes.cmp(&a.votes)));
+		// Remove duplicates
+		meme_votes.dedup();
+		// Sort by votes
+		meme_votes.sort_unstable_by(|a, b| b.votes.cmp(&a.votes));
+
+		// Remove memes if more than 10
+		if meme_votes.len() > 10 {
+			for index in (10..meme_votes.len()).rev() {
+				meme_votes.remove(index);
+			}
+		}
+
+		top_memes.set(&meme_votes);
 	}
 
 	#[view]
@@ -133,24 +178,25 @@ pub trait MemesVoting {
 
 	#[view]
 	#[storage_mapper("memeVotes")]
-	fn meme_votes(&self, nft_nonce: u64) -> SingleValueMapper<Self::Storage, u32>;
+	fn meme_votes(&self, nft_nonce: u64, period: u64) -> SingleValueMapper<Self::Storage, u32>;
 
 	#[view]
 	#[storage_mapper("periodTopMemes")]
-	fn period_top_memes(&self, period: u64) -> VecMapper<Self::Storage, (Meme, u32)>;
+	fn period_top_memes(&self, period: u64) -> SingleValueMapper<Self::Storage, Vec<MemeVotes>>;
+
+	#[view]
+	#[storage_mapper("addressVotes")]
+	fn address_votes(&self, address: Address) -> SingleValueMapper<Self::Storage, AddressVotes>;
 
 	#[storage_mapper("creatorContract")]
 	fn creator_contract(&self) -> SingleValueMapper<Self::Storage, Address>;
 
 	// Always needed
 	#[endpoint]
+	#[only_owner]
 	fn claim(&self) -> SCResult<()> {
 		let caller = self.blockchain().get_caller();
-		require!(
-			caller == self.blockchain().get_owner_address(),
-			"only owner can claim"
-		);
-		self.send().direct_egld(&caller, &self.blockchain().get_sc_balance(), b"claiming success");
+		self.send().direct_egld(&caller, &self.blockchain().get_sc_balance(&TokenIdentifier::egld(), 0), b"claiming success");
 		Ok(())
 	}
 }
--- memes-voting/src/meme.rs
+++ memes-voting/src/meme.rs
@@ -1,10 +1,29 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
+// TODO: Maybe remove some fields from this and instead rely on Elrond REST API?
 #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, TypeAbi)]
 pub struct Meme {
     pub owner: Address,
     pub nft_nonce: u64,
     pub created_at: u64,
     pub category: u8,
+}
+
+#[derive(TopEncode, TopDecode, TypeAbi)]
+pub struct AddressVotes {
+    pub period: u64,
+    pub votes: u8,
+}
+
+#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, TypeAbi)]
+pub struct MemeVotes {
+    pub nft_nonce: u64,
+    pub votes: u32,
+}
+
+impl PartialEq for MemeVotes {
+    fn eq(&self, other: &Self) -> bool {
+        self.nft_nonce == other.nft_nonce
+    }
 }
